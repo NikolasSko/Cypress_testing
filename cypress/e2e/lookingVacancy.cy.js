a2578d67ce5eb94eadd7b424bb7f32ce0541efb1
Nikita <Skobelev.nikita12345@mail.ru>
2024-03-20 17:07:31 +0700

    Тестирование отклика студентом и выхода из аккаунта

--- cypress/e2e/lookingVacancy.cy.js
+++ cypress/e2e/lookingVacancy.cy.js
@@ -7,11 +7,6 @@ describe('Просмотр вакансий', () => {
     cy.get('.form-input--password').type('Password1', {timeout: 3000});
     cy.get(':nth-child(3) > .button').click();
     cy.get(':nth-child(1) > .header__nav > [href="/vacancies"]').click();
-    cy.get('.form-input--text').type('Системный администратор', {force: true});
-    // cy.get(':nth-child(3) > :nth-child(2) > .form-select__selected').click();
-    cy.get('.form-select__items > :nth-child(2)').click();
-    cy.get(':nth-child(4) > :nth-child(2) > .form-select__selected').click();
-    cy.get('.form-select__items > :nth-child(2)').click();
-    cy.get('div.search-input__field > .button').click(); 
+
   })
 })
